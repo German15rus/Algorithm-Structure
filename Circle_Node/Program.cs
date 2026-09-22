7e0d9d88bb3c5ff294a404c5e8afb7e5f40e7c63
German <usasmen123@mail.ru>
2026-09-22 20:47:44 +0300

    ну я старался хотя бы

---
+++ Circle_Node/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Node
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList1 LinkList = new LinkedList1();
+
+            LinkList.AddFirst(new Node(0));
+            LinkList.Print();
+
+            LinkList.AddFirst(new Node(1));
+            LinkList.Print();
+
+            LinkList.AddFirst(new Node(2));
+            LinkList.Print();
+
+            LinkList.AddLast(new Node(3));
+            LinkList.Print();
+
+            LinkList.RemoveLast();
+
+            LinkList.Print();
+        }
+
+        public class Node
+        {
+            public Node Next;
+            public Node Prev;
+            public int Value;
+
+            public Node(int value)
+            {
+                Value = value;
+            }
+        }
+
+        public class LinkedList1
+        {
+            private Node _head;
+
+            public void AddFirst(Node node)
+            {
+                if (_head == null)
+                {
+                    _head = node;
+                    node.Next = node;
+                    node.Prev = node;
+                    return;
+                }
+
+                node.Next = _head;
+                node.Prev = _head.Prev;
+                _head.Prev = node;
+                _head.Prev.Next = node;
+                _head = node;
+            }
+
+            public void AddLast(Node node)
+            {
+                if (_head == null)
+                {
+                    _head = node;
+                    node.Next = node;
+                    node.Prev = node;
+                    return;
+                }
+
+                node.Next = _head;
+                node.Prev = _head.Prev;
+                _head.Prev.Next = node;
+                _head.Prev = node;
+            }
+
+            public void RemoveFirst()
+            {
+                if (_head == null)
+                {
+                    return;
+                }
+
+                if (_head.Next == null)
+                {
+                    _head = null;
+                    return;
+                }
+
+                _head.Next.Prev = _head.Prev;
+                _head.Prev.Next = _head.Next;
+                _head = _head.Next;
+            }
+
+            public void RemoveLast()
+            {
+                if (_head == null)
+                {
+                    return;
+                }
+
+                if (_head.Next == null)
+                {
+                    _head = null;
+                    return;
+                }
+
+                _head.Prev.Prev.Next = _head;
+                _head.Prev = _head.Prev.Prev;
+            }
+
+            public void Print()
+            {
+                Node curent = _head;
+
+                while (curent != null)
+                {
+                    Console.Write(curent.Value + " ");
+                    curent = curent.Next;
+
+                    if (curent == _head)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Node_tail/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Node_tail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList1 LinkList = new LinkedList1();
+
+            LinkList.AddFirst(new Node(0));
+            LinkList.AddFirst(new Node(1));
+            LinkList.AddFirst(new Node(2));
+
+            LinkList.AddLast(new Node(3));
+
+            LinkList.Print();
+        }
+        public class Node
+        {
+            public Node Next;
+            public int Value;
+
+            public Node(int value)
+            {
+                Value = value;
+            }
+        }
+
+        public class LinkedList1
+        {
+            private Node _head;
+            private Node _tail;
+
+            public void AddFirst(Node node)
+            {
+                node.Next = _head;
+                _head = node;
+
+                if (_tail == null)
+                {
+                    _tail = node;
+                }
+            }
+
+            public void AddLast(Node node)
+            {
+                if (_head == null)
+                {
+                    _head = node;
+                    return;
+                }
+
+                _tail.Next = node;
+                node = _tail;
+            }
+
+            public void RemoveFirst()
+            {
+                if (_head == null)
+                {
+                    return;
+                }
+
+                _head = _head.Next;
+            }
+
+            public void RemoveLast()
+            {
+                if (_head == null)
+                {
+                    return;
+                }
+                if (_head.Next == null)
+                {
+                    _head = null;
+                    return;
+                }
+
+                else
+                {
+                    Node curent = _head;
+
+                    while (curent.Next != _tail)
+                    {
+                        curent = curent.Next;
+                    }
+
+                    curent.Next = null;
+                }
+            }
+
+            public void Print()
+            {
+                Node curent = _head;
+
+                while (curent != null)
+                {
+                    Console.Write(curent.Value + " ");
+                    curent = curent.Next;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
